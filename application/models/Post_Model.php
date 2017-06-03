4415291fe35e5a5310b63965faacd1107218df08
neoalitcahya <neo.alit.cahya@live.com>
2017-06-03 18:09:13 +0700

    - add Count Komentar to all Get Post Model

--- application/models/Post_Model.php
+++ application/models/Post_Model.php
@@ -50,9 +50,12 @@ class Post_Model extends CI_Model {
     }  
 
     public function get_post_byIdUser($id_user){
-       $sql = "SELECT p.*, u.id as user_id, u.nama, u.foto as user_foto
-                FROM post p, users u
-                WHERE p.id_user = ? AND u.id = p.id_user
+       $sql = "SELECT p.*, u.id as user_id, u.nama, u.foto as user_foto, IFNULL(count(k.id),0) as count_komentar
+                FROM post p
+                LEFT JOIN users u ON p.id_user = u.id
+                LEFT JOIN komentar k ON k.id_post = p.id
+                WHERE p.id_user = ?
+                GROUP BY p.id
                 ORDER BY p.created_at DESC";
        $hasil = $this->db->query($sql, array($id_user));
        $post = $hasil->result_array();
@@ -73,7 +76,7 @@ class Post_Model extends CI_Model {
                 LEFT JOIN users u ON p.id_user = u.id
                 LEFT JOIN users_teman ut ON p.id_user = ut.id_user OR p.id_user = ut.id_user
                 LEFT JOIN komentar k ON k.id_post = p.id
-                WHERE p.id_user = 1
+                WHERE p.id_user = ?
                 GROUP BY p.id
                 ORDER BY p.created_at DESC";
        $hasil = $this->db->query($sql, array($id_user));
@@ -90,9 +93,12 @@ class Post_Model extends CI_Model {
     }
 
     public function get_post_byIdGrup($id_grup){
-       $sql = "SELECT p.*, u.id as user_id, u.nama, u.foto as user_foto
-                FROM post p, users u 
-                WHERE p.id_grup = ? AND p.id_user = u.id 
+       $sql = "SELECT p.*, u.id as user_id, u.nama, u.foto as user_foto, IFNULL(count(k.id),0) as count_komentar
+                FROM post p
+                LEFT JOIN users u ON p.id_user = u.id
+                LEFT JOIN komentar k ON k.id_post = p.id
+                WHERE p.id_grup = ?
+                GROUP BY p.id
                 ORDER BY p.created_at DESC";
        $hasil = $this->db->query($sql, array($id_grup));
        $post = $hasil->result_array();
