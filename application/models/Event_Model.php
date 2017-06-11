5c25594b09eb6629f4ee7bd0b358e211fe749ee2
neoalitcahya <neo.alit.cahya@live.com>
2017-06-11 20:46:05 +0700

    1. add ;

--- application/models/Event_Model.php
+++ application/models/Event_Model.php
@@ -14,18 +14,18 @@ class Event_Model extends CI_Model {
         $array = array($nama, $tanggal, $tempat, $hadiah1);
         if(isset($hadiah2)){
             $sql .= "`hadiah2`,";
-            $values .= "?,"
-            array_push($array, $hadiah2)
+            $values .= "?,";
+            array_push($array, $hadiah2);
         }
         if(isset($hadiah3)){
             $sql .= "`hadiah3`,";
-            $values .= "?,"
-            array_push($array, $hadiah3)
+            $values .= "?,";
+            array_push($array, $hadiah3);
         }
         if(isset($foto)){
             $sql .= "`foto`,";
-            $values .= "?,"
-            array_push($array, $foto)
+            $values .= "?,";
+            array_push($array, $foto);
         }
         $sql .= "`harga_tiket`, `deskripsi`, `id_user`, `id_kota`, `created_at`) ".  $values . "?,?,?,?,NOW());";
         array_push($array, $harga_tiket, $deskripsi, $id_user, $id_kota);
