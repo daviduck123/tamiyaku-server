1774064e860f14512f58122455d982fcc55d33b8
David <daviduck123@gmail.com>
2017-06-18 13:45:03 +0700

    - import UsersTeman_Model to controllers/api/User.php

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -8,6 +8,7 @@ class User extends REST_Controller {
     public function __construct(){
         parent::__construct();
         $this->load->model('User_Model');
+        $this->load->model('UsersTeman_Model');
     }
 
     public function index_get(){
